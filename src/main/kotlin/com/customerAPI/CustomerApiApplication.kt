d71b82530b970194c1c5dbb43002a54f1b342e92
natanPrs <natan.void@proton.me>
2025-10-20 16:59:28 -0300

    initial commit

---
+++ src/main/kotlin/com/customerAPI/CustomerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.customerAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CustomerApiApplication>(*args)
+}
