6429abff0e17a3303a3ac8e0b6fe706e8828a110
natanPrs <natan.void@proton.me>
2025-10-30 16:08:19 -0300

    feat: B solicitation

--- src/main/kotlin/com/customerAPI/controllers/UserController.kt
+++ src/main/kotlin/com/customerAPI/controllers/UserController.kt
@@ -3,8 +3,6 @@ package com.customerAPI.controllers
 import com.customerAPI.dtos.CreateCustomerDto
 import com.customerAPI.dtos.response.CreateCustomerResponseDto
 import com.customerAPI.services.UserService
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,9 +15,7 @@ class UserController(
 ) {
 
     @PostMapping("/create-user")
-    fun createUser(@RequestBody payload: CreateCustomerDto): ResponseEntity<CreateCustomerResponseDto> {
-        val user = userService.createUser(payload)
-        return ResponseEntity.status(HttpStatus.CREATED).body(user)
-    }
+    fun createUser(@RequestBody payload: CreateCustomerDto): CreateCustomerResponseDto =
+        userService.createUser(payload)
 
 }
